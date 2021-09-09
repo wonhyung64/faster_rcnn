63ac492d24646f5f426575ae35b37192c29b2834
wonhyung64 <64861508+wonhyung64@users.noreply.github.com>
2021-09-09 12:44:10 +0900

    9/8

--- attempt1.py
+++ attempt1.py
@@ -59,6 +59,11 @@ for data in val_data.take(1):
 
 hyper_params["total_labels"] = len(labels) + 1
 
+#%%
+# for image_data in train_data.take(1):
+#     img = image_data['image']
+#     gt_boxes = image_data['objects']['bbox']
+#     gt_labels = tf.cast(image_data['objects']['label'] + 1, tf.int32)
 #%% DATA PREPROCESSING DEF
 def preprocessing(image_data, final_height, final_width, apply_augmentation=False, evaluate=False):
     img = image_data['image']
@@ -161,8 +166,8 @@ base_anchors = tf.cast(base_anchors, dtype=tf.float32)
 print(base_anchors)
 
 anchors = tf.reshape(base_anchors, (1, -1, 4)) + tf.reshape(grid_map, (-1, 1, 4))
-print(tf.reshape(base_anchors, (1, -1, 4)))
-print(tf.reshape(grid_map, (1, -1, 4)))
+# print(tf.reshape(base_anchors, (1, -1, 4)))
+# print(tf.reshape(grid_map, (1, -1, 4)))
 print(anchors)
 
 anchors = tf.reshape(anchors, (-1, 4))
@@ -170,6 +175,9 @@ print(anchors)
 anchors = tf.clip_by_value(t=anchors, clip_value_min=0, clip_value_max=1) # tf.clip_by_value : min, max값보다 작거나 같은 값을 clip 값으로 대체
 print(anchors)
 
+#%%
+# for image_data in train_data.take(1):
+#     img, gt_boxes, gt_labels = image_data
 #%% Generating Region Proposal DEF
 def rpn_generator(dataset, anchors, hyper_params):
     while True:
@@ -180,7 +188,7 @@ def rpn_generator(dataset, anchors, hyper_params):
 
 #%%
 def calculate_rpn_actual_outputs(anchors, gt_boxes, gt_labels, hyper_params):
-    batch_size = tf.shape(gt_boxes)[0] # gt_boxes 는 [이미지 데이터 한장에 있는 라벨의 갯수(n개) , 좌표(4)]
+    batch_size = tf.shape(gt_boxes)[0] # gt_boxes 는 [batch_size, 이미지 데이터 한장에 있는 라벨의 갯수(4개) , 좌표(4)]
     feature_map_shape = hyper_params['feature_map_shape'] # feature_map_shape = 31
     anchor_count = hyper_params['anchor_count'] # anchor_count = 3 * 3 
     total_pos_bboxes = hyper_params['total_pos_bboxes'] # 이게 뭘 나타내는건지 알 수 없음.. 128
@@ -208,10 +216,10 @@ def calculate_rpn_actual_outputs(anchors, gt_boxes, gt_labels, hyper_params):
     # 8장의 사진에 대한, C X C X 9 개의 reference anchors 와, ground truth box n개(최대4) 와의 IoU계산
     #
     max_indices_each_row = tf.argmax(iou_map, axis=2, output_type=tf.int32)
-    # 각 사진에서 IoU가 가장 높게 나오는 gt_box의 reference anchor에서의 index 추출
+    # 각 사진의 reference anchor 에서 IoU가 가장 높게 나오는 gt_box의 index 추출 (8, 8649, 1)
 
     max_indices_each_column = tf.argmax(iou_map, axis=1, output_type=tf.int32)
-    # 각 사진에서 IoU가 가장 높게 나오는 reference anchor 의 index 추출
+    # 각 사진의 gt_box에서 IoU가 가장 높게 나오는 reference anchor 의 index 추출 (8, 1, 4)
     #
     merged_iou_map = tf.reduce_max(iou_map, axis=2) 
     # 8장의 사진 에서 하나의 reference anchor와 4개의 gt_boxes 들 중 가장 높은값만 남기기
@@ -220,7 +228,7 @@ def calculate_rpn_actual_outputs(anchors, gt_boxes, gt_labels, hyper_params):
     # 각 사진에서 가장 높은 IoU가 threshold 보다 높은지 
     #
     valid_indices_cond = tf.not_equal(gt_labels, -1)
-    # 왜 -1?
+    # 왜 -1? : gt_labels 중 라벨이 없는 값은 -1 로 입력
     
     valid_indices = tf.cast(tf.where(valid_indices_cond), tf.int32)
     # valid_indices 에서 label이 있는 부분의 tensor_index 반환
@@ -231,17 +239,18 @@ def calculate_rpn_actual_outputs(anchors, gt_boxes, gt_labels, hyper_params):
 
     scatter_bbox_indices = tf.stack([valid_indices[..., 0], valid_max_indices], 1)
     # 8장의 사진 에서 라벨이 존재하는 사진의 index와 해당 라벨의 gt_box와 가장 높은 IoU를 가지는 reference acnhor의 index 반환 
-    max_pos_mask = tf.scatter_nd(scatter_bbox_indices, tf.fill((tf.shape(valid_indices)[0], ), True), tf.shape(pos_mask))
-    # 8장의 사진 각각의 reference anchors에서 gt_box와의 IoU 를 가지는 reference anchor의 index 만 True
+    max_pos_mask = tf.scatter_nd(indices=scatter_bbox_indices, updates=tf.fill((tf.shape(valid_indices)[0], ), True), shape=tf.shape(pos_mask))
+    # 8장의 사진 각각의 reference anchors에서 gt_box와의 가장 높은 IoU 를 가지는 reference anchor의 index 만 True
     pos_mask = tf.logical_or(pos_mask, max_pos_mask)
     # pos_mask에 threshold 이상의 IoU를 가지는 reference anchor 와, 가장 높은 IoU를 가지는 reference anchor 만 True 반환
     pos_mask = randomly_select_xyz_mask(pos_mask, tf.constant([total_pos_bboxes], dtype=tf.int32))
     #
     pos_count = tf.reduce_sum(tf.cast(pos_mask, tf.int32), axis=-1)
-
+    # 8장의 pos_mask 에 대해서 각각 true의 개수 반환
     neg_count = (total_pos_bboxes + total_neg_bboxes) - pos_count
     
     neg_mask = tf.logical_and(tf.less(merged_iou_map, 0.3), tf.logical_not(pos_mask))
+    # 8장의 사진에서 Iou가 0.3보다 작고 pos_mask가 false인 부분만 False
     neg_mask = randomly_select_xyz_mask(neg_mask, neg_count)
     #
     
@@ -249,11 +258,12 @@ def calculate_rpn_actual_outputs(anchors, gt_boxes, gt_labels, hyper_params):
     # ?
     neg_labels = tf.cast(neg_mask, dtype=tf.float32)
     bbox_labels = tf.add(pos_labels, neg_labels)
-    
-    gt_boxes_map = tf.gather(gt_boxes, max_indices_each_row, batch_dims=1)
+    # 정리 자료에서 cls loss의 p*_i
+    gt_boxes_map = tf.gather(params=gt_boxes, indices=max_indices_each_row, batch_dims=1)
+    # 8장의 사진에서 8649개의 reference anchor 중 4개의 gt_box와 IoU가 가장 높은 IoU값 4개의 gt_box에 대해 각각 반환
 
     expanded_gt_boxes = tf.where(tf.expand_dims(pos_mask, -1), gt_boxes_map, tf.zeros_like(gt_boxes_map))
-    
+    # 정리 자료에서 reg loss의 p*_i
     
     bbox_width = anchors[..., 3] - anchors[..., 1]
     bbox_height = anchors[..., 2] - anchors[...,0]
@@ -273,11 +283,13 @@ def calculate_rpn_actual_outputs(anchors, gt_boxes, gt_labels, hyper_params):
     delta_h = tf.where(tf.equal(gt_height, 0), tf.zeros_like(gt_height), tf.math.log(gt_height / bbox_height))
     
     bbox_deltas = tf.stack([delta_y, delta_x, delta_h, delta_w], axis=-1) / variances
-
+    bbox_deltas = tf.reshape(bbox_deltas, (batch_size, feature_map_shape, feature_map_shape, anchor_count * 4))
     bbox_labels = tf.reshape(bbox_labels, (batch_size, feature_map_shape, feature_map_shape, anchor_count))
     
     return bbox_deltas, bbox_labels
 #%%
+# select_xyz = neg_count
+# mask = neg_mask
 def randomly_select_xyz_mask(mask, select_xyz):
     maxval = tf.reduce_max(select_xyz) * 10
     random_mask = tf.random.uniform(tf.shape(mask), minval=1, maxval=maxval, dtype=tf.int32)
@@ -329,37 +341,38 @@ rpn_model.summary()
 
 #%% Regression Loss Function
 def rpn_reg_loss(*args):
-    y_true, y_pred = args if len(args) == 2 else args[0]
-    y_pred = tf.reshape(y_pred, shape=[tf.shape(y_pred)[0], -1, 4])
+    y_pred, y_true = args if len(args) == 2 else args[0]
     #
     loss_fn = tf.losses.Huber(reduction=tf.losses.Reduction.NONE)
     # Huber : SmoothL1 loss function
 
-    loss_for_all = loss_fn(y_true, y_pred)
+    loss_for_all = loss_fn(y_true[0], y_pred[0])
     loss_for_all = tf.reduce_sum(loss_for_all, axis=-1)
     # sum of SmoothL1
     
-    pos_cond = tf.reduce_any(tf.not_equal(y_true, tf.constant(0.0)), axis=-1)
+    pos_cond = tf.reduce_any(tf.not_equal(y_true[1], tf.constant(-1.0)), axis=-1)
     # tf.reduce_any?
     
     pos_mask = tf.cast(pos_cond, dtype=tf.float32)
-    # p*_i
+    # positive label
     
     #
     loc_loss = tf.reduce_sum(pos_mask * loss_for_all)
 
     total_pos_bboxes = tf.maximum(1.0, tf.reduce_sum(pos_mask))
 
-    return loc_loss / total_pos_bboxes
+    learning_rate = 10
+
+    return loc_loss / total_pos_bboxes * learning_rate
 
 #%% Objectness Loss Function
 def rpn_cls_loss(*args):
-    y_true, y_pred = args if len(args) == 2 else args[0]
+    y_pred, y_true = args if len(args) == 2 else args[0]
 
-    indices = tf.where(tf.not_equal(y_true, tf.constant(-1.0, dtype=tf.float32)))
+    indices = tf.where(tf.not_equal(y_true[1], tf.constant(0.0, dtype=tf.float32)))
 
-    target = tf.gather_nd(y_true, indices)
-    output = tf.gather_nd(y_pred, indices)
+    target = tf.gather_nd(y_true[1], indices)
+    output = tf.gather_nd(y_pred[1], indices)
     # tf.gather_nd ?
     
     lf = tf.losses.BinaryCrossentropy()
@@ -385,16 +398,21 @@ step_size_train = math.ceil(train_total_items / batch_size)
 step_size_val = math.ceil(val_total_items / batch_size)
 
 #%%
-tmp = next(iter(rpn_train_feed))
+# tmp = next(iter(rpn_train_feed))
 rpn_model.fit(rpn_train_feed,
               steps_per_epoch=step_size_train,
               validation_data=rpn_val_feed,
               validation_steps=step_size_val,
               epochs=epochs)
 # %%
-img, (bbox_deltas, bbox_labels) = next(iter(rpn_train_feed))
-y_true, y_pred = args if len(args) == 2 else args[0]
-y_true = bbox_deltas
-y_pred = rpn_model(img)
-y_pred[1]
-rpn_reg_loss(y_true, y_pred[1])
+# img, (bbox_deltas, bbox_labels) = next(iter(rpn_train_feed))
+# # y_, y_pred = args if len(args) == 2 else args[0]
+# y_pred = img 
+# y_true = (bbox_deltas, bbox_labels)
+# y_pred[1]
+# rpn_reg_loss(y_true, y_pred)
+
+
+
+
+
