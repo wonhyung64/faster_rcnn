32902b2b700e21cfce6f90e6c86a496134f59908
an-seunghwan <wonhyung64@icloud.com>
2022-07-11 17:20:12 +0900

    feat - sh

---
+++ run_coco17.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=hgx
+##
+#SBATCH --job-name=frcnn_coco17
+#SBATCH -o SLURM.%N.%j.out
+#SBATCH -e SLURM.%N.%j.err
+##
+#SBATCH --gres=gpu:hgx:1
+
+hostname
+date
+
+module add CUDA/11.2.2
+module add ANACONDA/2020.11
+
+python /home1/wonhyung64/Github/faster_rcnn/main.py --data-dir /home1/wonhyung64/data --name coco/2017
