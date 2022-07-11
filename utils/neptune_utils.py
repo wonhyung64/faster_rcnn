81c92bf708298814fadbba13abc3f1f7b6d81f2a
an-seunghwan <wonhyung64@icloud.com>
2022-07-11 16:49:58 +0900

    feat - neptune settings

--- utils/neptune_utils.py
+++ utils/neptune_utils.py
@@ -28,11 +28,12 @@ def record_train_loss(run, loss_rpn, loss_dtn, total_loss):
     run["train/loss/total_loss"].log(total_loss.numpy())
 
 
-def record_result(run, weights_dir, mean_ap, train_time, mean_test_time):
+def record_result(run, weights_dir, train_time, mean_ap, mean_test_time):
     res = {
         "mean_ap": "%.3f" % (mean_ap.numpy()),
         "train_time": train_time,
         "inference_time": "%.2fms" % (mean_test_time.numpy()),
     }
     run["results"] = res
-    run["model"].upload(weights_dir)
+    run["rpn_model"].upload(f"{weights_dir}_rpn.h5")
+    run["dtn_model"].upload(f"{weights_dir}_dtn.h5")
