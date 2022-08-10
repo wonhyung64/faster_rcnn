2d5d2630dd16083c2cd5b6d9859b00f1afaaea29
an-seunghwan <wonhyung64@icloud.com>
2022-08-10 20:36:48 +0900

    hotfix - draw label errors

--- utils/draw_utils.py
+++ utils/draw_utils.py
@@ -30,31 +30,32 @@ def draw_dtn_output(
     labels,
     final_labels,
     final_scores,
+    colors,
 ):
     image = tf.squeeze(image, axis=0)
+    final_bboxes = tf.squeeze(final_bboxes, axis=0)
+    final_labels = tf.squeeze(final_labels, axis=0)
+    final_scores = tf.squeeze(final_scores, axis=0)
+
     image = tf.keras.preprocessing.image.array_to_img(image)
     width, height = image.size
     draw = ImageDraw.Draw(image)
 
-    y1 = final_bboxes[0][..., 0] * height
-    x1 = final_bboxes[0][..., 1] * width
-    y2 = final_bboxes[0][..., 2] * height
-    x2 = final_bboxes[0][..., 3] * width
+    y1 = final_bboxes[..., 0] * height
+    x1 = final_bboxes[..., 1] * width
+    y2 = final_bboxes[..., 2] * height
+    x2 = final_bboxes[..., 3] * width
 
     denormalized_box = tf.round(tf.stack([y1, x1, y2, x2], axis=-1))
-
-    colors = tf.random.uniform((len(labels), 4), maxval=256, dtype=tf.int32)
 
     for index, bbox in enumerate(denormalized_box):
         y1, x1, y2, x2 = tf.split(bbox, 4, axis=-1)
         width = x2 - x1
         height = y2 - y1
 
-        final_labels_ = tf.reshape(final_labels[0], shape=(200,))
-        final_scores_ = tf.reshape(final_scores[0], shape=(200,))
-        label_index = int(final_labels_[index])+1
+        label_index = int(final_labels[index])
         color = tuple(colors[label_index].numpy())
-        label_text = "{0} {1:0.3f}".format(labels[label_index], final_scores_[index])
+        label_text = "{0} {1:0.3f}".format(labels[label_index], final_scores[index])
         draw.text((x1 + 4, y1 + 2), label_text, fill=color)
         draw.rectangle((x1, y1, x2, y2), outline=color, width=3)
 
